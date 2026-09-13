5562cfc5041bc27514c3784c45a739d448410e19
Joy-Majumder <gopalmajumdarjoy@gmail.com>
2026-09-13 17:03:44 +0600

    models and trained things

---
+++ train.py
@@ -0,0 +1,4 @@
+from ultralytics import YOLO
+
+model = YOLO("/Users/joy0x1/Downloads/UIU Mariner/WORK/DataSet/model/yolo26x.pt")
+model.train(data = "/Users/joy0x1/Downloads/UIU Mariner/WORK/DataSet/dataset/Custom_dataset.yaml", imgsz=640,batch= 9, epochs = 100, workers = 0, device= 0 )
