3f814472a5b27ad07b0815a4d63bc2638052014f
Joy-Majumder <gopalmajumdarjoy@gmail.com>
2026-09-16 22:10:22 +0600

    Hello WOrld Updated

--- SourceCodes/detect.py
+++ SourceCodes/detect.py
@@ -1,8 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO("/Users/joy0x1/Downloads/UIU Mariner/WORK/DataSet/CustomModel/CustomModelVersion2.pt") # with this model v2 0.8 cant detect well 0.7 did perfectly
+# model = YOLO("/Users/joy0x1/Downloads/UIU Mariner/WORK/DataSet/CustomModel/CustomModelVersion2.pt") # with this model v2 0.8 cant detect well 0.7 did perfectly
+model = YOLO("/Users/joy0x1/Downloads/UIU Mariner/WORK/DataSet/CustomModel/best.pt")
 # model.predict(source="CamImage/1.png", show=True, save=True,conf=0.8, save_crop=True)
-model.predict(source="/Users/joy0x1/Downloads/UIU Mariner/WORK/DataSet/videoSource/1.mp4",show=True, conf=0.01,
+model.predict(source="/Users/joy0x1/Downloads/UIU Mariner/WORK/DataSet/videoSource/1.mp4",show=True, conf=0.6,
     imgsz=640,
     save=True,
     verbose=True)
